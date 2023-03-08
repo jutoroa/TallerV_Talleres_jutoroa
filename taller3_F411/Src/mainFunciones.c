87123b564d19958344d43e66b782cbddd9813c20
Juan Pablo Toro Arenas <jutoroa@unal.edu.co>
2023-03-07 23:50:01 -0500

    Update Taller 5 - 2023_01

--- taller3_F411/Src/mainFunciones.c
+++ taller3_F411/Src/mainFunciones.c
@@ -19,8 +19,24 @@
 // El main no requiere prototipo.
 // El main, debe ser la primera función del programa.
 
+bool primeraVariableGlobal = true;
 void miPrimeraFuncion(void);
+
 uint8_t getMaxChar(void);
+
+// Explicación IF
+uint16_t getMaxValue(uint16_t x, uint16_t y, uint16_t z);
+
+// Primer ejercicio:
+/* Crear una función que tenga dos argumentos (parámetros de entrada). El primero, tendrá el valor
+ * correspondiente al número de bits de la variable. El segundo, representa si la variable tiene signo
+ * o no.
+ */
+
+uint64_t getMaxBits(uint8_t nBits, uint8_t signo);
+
+#define UNSIGNED 0
+#define SIGNED 	 1
 
 int main(void)
 {
@@ -29,99 +45,68 @@ int main(void)
 	uint8_t charMax = getMaxChar();
 	(void) charMax;
 
-	while(1){
+	// Ejemplo básico if
+	uint16_t maxValue = getMaxValue(150,0b00101111,0xFEA);
 
-		// Ejemplo básico if
+	// Primer ejercicio
+	uint64_t maxBits = getMaxBits(16, UNSGINED);
+	maxBits = getMaxBits(16, SIGNED);
+	maxBits = getMaxBits(13, UNSIGNED);
 
-		uint8_t estatura = 0;
-		bool puedeIngresar;
+	// Ejemplo for básico
 
-		if(estatura >= 150){
-			puedeIngresar = true;
-		}
-		else{
-			puedeIngresar = false;
-		}
+	// Tabla del siete
 
-		// Ejemplo else if anidado
+	uint8_t resultado = 0;
 
-		uint32_t dineroEnLaCuenta = 400000;
-		bool tieneCarro = true;
-
-		if(dineroEnLaCuenta < 1000000){
-			if(tieneCarro == false){
-				char hermosura[] = "FEO";
-			}
-			else{
-				char hermosura[] = "Feo, pero le acepto la salida";
-				int n = sizeof(hermosura);
-				int a = 1+2;
-			}
-		}
-		else if(dineroEnLaCuenta >= 1000000 && dineroEnLaCuenta < 5000000){
-			if(tieneCarro == false){
-				char hermosura[] = "Breve";
-			}
-			else{
-				char hermosura[] = "Le roto el instagram";
-			}
-		}
-		else if(dineroEnLaCuenta >= 5000000 && dineroEnLaCuenta < 10000000){
-			char hermosura[] = "Bonito";
-		}
-		else{
-			char hermosura[] = "Me caso";
-		}
-
-
-
-		// Ejemplo for básico
-
-		// Tabla del siete
-
-		uint8_t resultado = 0;
-
-		for(uint8_t contador = 0; contador < 10; contador ++){
-			resultado = 7 * contador;
-		}
-
-		// Ejemplo for anidado Cronómetro
-
-		for(uint8_t horas = 0; horas < 24; horas ++){
-			// For para los minutos
-			for(uint8_t minutos = 0; minutos < 60; minutos++){
-				// For para los segundos
-				for(uint8_t segundos = 0; segundos < 60; segundos++){
-					// Observar Variables en el debugger
-					NOP();
-				}
-			}
-		}
-
-		// Ejercicio de clase
-
-		// Crear un conversor de 8 bits binarios, a decimal	haciendo uso de los ciclos For
-		// y de la librería math.h
-
-		uint8_t x, var, dec;
-		x = 0b11101110;
-
-		for (int i=0; i<8; i++){
-			var=x&(1<<i);
-			var=var>>i;
-			dec=dec+(var*pow(2, i));
-		}
-
-		// Crear un conversor de n bits binarios, a decimal	haciendo uso de los ciclos For
-		// y de la librería math.h
-
-		for (int i=0; i< (sizeof(x)*8); i++){
-			var=x&(1<<i);
-			var=var>>i;
-			dec=dec+(var*pow(2, i));
-		}
-
+	for(uint8_t contador = 0; contador < 10; contador ++){
+		resultado = 7 * contador;
 	}
+
+	// Ejemplo for anidado Taxímetro 3 dígitos
+
+	for(uint8_t centenas = 0; centenas < 10; centenas ++){
+		// For para las decenas
+		for(uint8_t decenas = 0; decenas < 10; decenas++){
+			// For para las unidades
+			for(uint8_t unidades = 0; unidades < 10; unidades++){
+				// Observar Variables en el debugger
+				NOP();
+			}
+		}
+	}
+
+	// Ejercicio de clase
+
+	// Crear un conversor de 8 bits binarios, a decimal	haciendo uso de los ciclos For
+	// y de la librería math.h
+
+	uint8_t bin, var, dec = 0;
+	bin = 0b11101110;
+
+	for (int i=0; i<8; i++){
+		var = bin & (1<<i);
+		var = var>>i;
+		dec = dec + (var*pow(2, i));
+	}
+
+	// Crear un conversor de decimal a binario, haciendo uso de operadores BitWise y ciclo FOR
+	for(int i=0; i<8;i++){
+		var = (dec%2);
+		dec /= 2;
+		bin |= var<<i;
+	}
+
+	// Crear un conversor de n bits binarios, a decimal	haciendo uso de los ciclos For
+	// y de la librería math.h
+
+	for (int i=0; i< (sizeof(x)*8); i++){
+		var=x&(1<<i);
+		var=var>>i;
+		dec=dec+(var*pow(2, i));
+	}
+
+
 }
 
 // Definicion Funciones
@@ -133,25 +118,40 @@ int main(void)
 void miPrimeraFuncion(void){
 
 	// Manejo de variables booleanas
-
-	uint8_t miPrimeraBandera = true;
-	uint8_t miSegundaBandera = false;
-
-	uint8_t XOR = miPrimeraBandera ^ miSegundaBandera;
-	uint8_t OR  = miPrimeraBandera || miSegundaBandera;
-	uint8_t AND = miPrimeraBandera && miSegundaBandera;
-
-	XOR ^= 1;
-	XOR ^= 1;
+	primeraVariableGlobal = false;
 }
 
 uint8_t getMaxChar(void){
 
 	uint8_t charBits = 8;
-
 	uint8_t maxChar = (2 * 2 * 2 * 2 * 2 * 2 * 2 * 2) - 1;
+	maxChar = pow(2, charBits) - 1;
 
 	return maxChar;
-
 }
 
+uint16_t getMaxValue(uint16_t x, uint16_t y, uint16_t z){
+	if((x >= y) && (x >= z)){
+		return x;
+	}
+	else if((y >= x) && (y >= z)){
+		return y;
+	}
+	else{
+		return z;
+	}
+}
+
+getMaxBits(uint8_t nBits, uint8_t signo){
+	if((nBits == 8)||(nBits == 16)||(nBits == 32)||(nBits==64)){
+		if(signo == UNSIGNED){
+			return (pow(2,nBits)-1);
+		}
+		else{
+			return (pow(2,nBits)/2)-1;
+		}
+	}
+	else{
+		return false;
+	}
+}
